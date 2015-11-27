0e8eb45b4e5b1de3300b9f79beda5bb9eab438d8
streeterla <kaffee@streeter.at>
2015-11-27 21:03:00 +0100

    temp commit

--- src/main/java/de/sms/tabletennis/daos/PlayerDAO.java
+++ src/main/java/de/sms/tabletennis/daos/PlayerDAO.java
@@ -1,12 +1,15 @@
 package de.sms.tabletennis.daos;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import de.sms.tabletennis.entities.Player;
 
+@RepositoryRestResource(path = "player")
 public interface PlayerDAO extends CrudRepository<Player, Long> {
 	
 	public Iterable<Player> findAll();
 	
-	public Iterable<Player> findByFirstNameAndLastName(String firstName, String lastName);
+	public Iterable<Player> findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
